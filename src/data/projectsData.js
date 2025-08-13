c3892bc26b8ef0eba776ef33e9b5be252eb6052b
Habeebah Aleilo <habeebahdistinguishedmom@gmail.com>
2025-08-13 15:53:03 +0300

    Create and set up projects and skills data

---
+++ src/data/projectsData.js
@@ -0,0 +1,21 @@
+// data/projectsData.js
+export default [
+  {
+    title: "Feedback App",
+    description: "A full-stack MERN app to manage daily tasks with authentication CRUD.",
+    demo: "https://feedback-app-delta-coral.vercel.app",
+    code: "https://github.com/queenhabeebah/Feedback-App"
+  },
+  {
+    title: "Task Manager App",
+    description: "A full-stack MERN app to manage daily tasks with CRUD funstionality.",
+    demo: "https://task-manager-frontend-ten-theta.vercel.app/",
+    code: "https://github.com/queenhabeebah/Task-Manager-App"
+  },
+  {
+    title: "Blog App (In Progress)",
+    description: "A MERN stack blog platform with CRUD functionality.",
+    demo: "#",
+    code: "https://github.com/queenhabeebah/Blog-App"
+  }
+];
---
+++ src/data/skillsData.js
@@ -0,0 +1,5 @@
+export default [
+  "HTML", "CSS", "JavaScript", "React.js",
+  "Node.js", "Express.js", "MongoDB",
+  "Git & GitHub", "REST APIs", "Responsive Design"
+];
