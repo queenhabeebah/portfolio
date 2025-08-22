a0a17b3cf7a4092e8e200e13dde18b1576f78d4f
Habeebah Aleilo <habeebahdistinguishedmom@gmail.com>
2025-08-22 18:11:55 +0300

    Update contact info

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -6,7 +6,7 @@ function Contacts() {
     <section id="contact" className="contact-info">
         <h3>Get in Touch</h3>
         <ul>
-            <li><span>Email:</span> <a href="mailto:habeebahaleilo@gmail.com" target="_blank">habeebahaleilo@gmail.com</a></li>
+            <li><span>Email:</span> <a href="mailto:work.habeebahaleilo.dev@gmail.com" target="_blank">habeebahaleilo@gmail.com</a></li>
             <li><span>GitHub:</span> <a href="https://github.com/queenhabeebah" target="_blank">github.com/queenhabeebah</a></li>
             <li><span>LinkedIn:</span> <a href="https://linkedin.com/in/habeebah-aleilo" target="_blank">linkedin.com/in/habeebah-aleilo</a></li>
         </ul>
