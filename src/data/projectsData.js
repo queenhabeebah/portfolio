a663d97d86b849436d1b716ddff64221ed95a875
Habeebah Aleilo <habeebahdistinguishedmom@gmail.com>
2025-09-04 19:36:08 +0300

    Update public projectsData

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,5 +1,35 @@
 export default [
   {
+    title: "Blog App (In Progress)",
+    description:
+      "Built a blogging application with authentication, post management, and commenting system.",
+      features: [
+        "User authentication (signup, login, logout)",
+        "CRUD operations on blog posts",
+        "Commenting system",
+        "Image uploads",
+        "Authorization for secure content management",
+      ],
+      stack: ["React", "Node.js", "Express", "MongoDB", "Tailwind", "CSS"],
+      image: "blog-app-mockup.png" ,
+      demo: "https://blog-app-virid-one.vercel.app/",
+      code: "https://github.com/queenhabeebah/Blog-App",
+    },
+    {
+      title: "Task Manager App",
+      description:
+      "Developed a task management tool that lets users organize tasks by status and priority.",
+      features: [
+        "CRUD operations on tasks",
+        "Task status tracking (pending, in-progress, completed)",
+        "Priority levels for better organization",
+      ],
+      stack: ["React", "Node.js", "Express", "MongoDB", "Bootstrap", "CSS"],
+      image: "task-manager-app-mockup2.png",
+      demo: "https://task-manager-frontend-ten-theta.vercel.app/",
+      code: "https://github.com/queenhabeebah/Task-Manager-App",
+    },
+  {
     title: "Feedback App",
     description:
       "Built a web application that lets users register, log in, and leave feedback....",
@@ -12,35 +42,5 @@ export default [
     image: "feedback-app-mockup.png",
     demo: "https://feedback-app-delta-coral.vercel.app",
     code: "https://github.com/queenhabeebah/Feedback-App",
-  },
-  {
-    title: "Task Manager App",
-    description:
-      "Developed a task management tool that lets users organize tasks by status and priority.",
-    features: [
-      "CRUD operations on tasks",
-      "Task status tracking (pending, in-progress, completed)",
-      "Priority levels for better organization",
-    ],
-    stack: ["React", "Node.js", "Express", "MongoDB", "Bootstrap", "CSS"],
-    image: "task-manager-app-mockup.png",
-    demo: "https://task-manager-frontend-ten-theta.vercel.app/",
-    code: "https://github.com/queenhabeebah/Task-Manager-App",
-  },
-  {
-    title: "Blog App (In Progress)",
-    description:
-      "Built a blogging application with authentication, post management, and commenting system.",
-    features: [
-      "User authentication (signup, login, logout)",
-      "CRUD operations on blog posts",
-      "Commenting system",
-      "Image uploads",
-      "Authorization for secure content management",
-    ],
-    stack: ["React", "Node.js", "Express", "MongoDB", "Tailwind", "CSS"],
-    image: "blog-app-mockup.png" ,
-    demo: "https://blog-app-virid-one.vercel.app/",
-    code: "https://github.com/queenhabeebah/Blog-App",
-  },
+  }
 ];
